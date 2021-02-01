9590ace3b8a7f433763e6e9b9431c43ca2cd781f
cesar3963 <67514872+cesar3963@users.noreply.github.com>
2021-02-01 18:51:42 -0500

    lol we got it

--- app.py
+++ app.py
@@ -5,41 +5,19 @@ from flask import Flask, render_template,jsonify, request
 # from keras.models import load_model
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 app = Flask(__name__)
 
-load model
-model = load_model('model/model.h5')
-summarize model.
+# load model
+model = keras.models.load_model('model/model.h5')
+# summarize model
 model.summary()
 
-from sklearn.preprocessing import MinMaxScaler
+# from sklearn.preprocessing import MinMaxScaler
 
-@app.route("/submit", methods=['GET', 'POST'])
-# def index():
-    
-    """Return the homepage."""
-    if request.method == 'POST':
-       mushroom = request.form['mushroom']
-        data = {'cap-shape':  [, ,]
-            'cap-surface': [,],
-            'cap-color': [, ,
-            'odor': [, ],
-            'spore-print-color': [, ]
-            
-        }
-
-         df = pd.DataFrame (data, columns = ['cap-shape', 'cap-surface', 'cap-color', 'odor', 'spore-print-color'])
-        X_scaler = MinMaxScaler().fit(df)
-        test_scaled = X_scaler.transform(df)
-        encoded_predictions= model.predict_classes(test_scaled[])
-        if encoded_predictions[0] == 0:
-            final_prediction = "Likely Poisionous"
-        else: 
-            final_prediction = "Likely Safe"
-        return render_template('index.html', prediction = final_prediction)
-    else:
-        return render_template('index.html')
 
 @app.route("/")
 def index():
@@ -55,29 +33,25 @@ def about():
 def test():
     return render_template("test.html")
 
-
 @app.route("/graphs")
 def graphs():
     return render_template("graphs.html")
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET','POST'])
 def submit():
     if request.method == 'POST':
-        customer = request.form['customer']
-        dealer = request.form['dealer']
-        rating = request.form['rating']
-        comments = request.form['comments']
-        # print(customer, dealer, rating, comments)
-        if customer == '' or dealer == '':
-            return render_template('index.html', message='Please enter required fields')
-        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
-            data = Feedback(customer, dealer, rating, comments)
-            db.session.add(data)
-            db.session.commit()
-            send_mail(customer, dealer, rating, comments)
-            return render_template('success.html')
-        return render_template('index.html', message='You have already submitted feedback')
+        shape = request.form['shape']
+        surface = request.form['surface']
+        color = request.form['color']
+        odor = request.form['odor']
+        print = request.form['print']
+        population = request.form['population']
+        arr= np.array(([[shape,surface,color,odor,print,population]]))
+        pred = model.predict(arr)
+        return render_template('index.html', data=pred)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)    
+        
+        
