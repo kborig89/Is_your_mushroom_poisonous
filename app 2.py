e47a9b90eea7b20d998b041a7c21000b08ca6985
kathrynbuckwalter <kborig89@gmail.com>
2021-01-31 21:24:48 -0500

    app2 updated for forms

--- app 2.py
+++ app 2.py
@@ -5,36 +5,38 @@ from flask import Flask, jsonify, render_template, request
 from keras.models import load_model
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
 
-# load model
-model = load_model("mushrooms_deeplearn2.h5")
-# summarize model.
-model.summary()
 
-from sklearn.preprocessing import MinMaxScaler
+# load model
+deep_learning_model = load_model('model/mushrooms_deeplearn2.h5')
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     
     """Return the homepage."""
     if request.method == 'POST':
-       mushroom = request.form['mushroom']
-        data = {'cap-shape': ["Answer 1"],
-        'cap-surface': [0],
-        'cap-color': [5],
-        'odor': [5],
-        'spore-print-color': [0],
-        'population': [0]
-        }
-
         cap-shape = request.form['cap-shape']
+        cap-surface = request.form['cap-surface']
+        cap-color = request.form['cap-color']
+        odor = request.form['odor']
+        spore-print-color = request.form['spore-print-color']
+        population = request.form['population']
+
+        data = {'cap-shape':  [5.8, cap-shape],
+                'cap-surface': [0.300, cap-surface],
+                'cap-color': [0.33, cap-color],
+                'odor': [3.5, odor],
+                'spore-print-color': [0.033, spore-print-color],
+                'population': [25.0, population],
+                }
     
 
 
-
-         df = pd.DataFrame (data, columns = ['cap-shape', 'cap-surface', 'cap-color', 'odor', 'spore-print-color'])
+         df = pd.DataFrame (data, columns = ['cap-shape', 'cap-surface', 'cap-color', 'odor', 'spore-print-color', 'population'])
         X_scaler = MinMaxScaler().fit(df)
         test_scaled = X_scaler.transform(df)
         encoded_predictions= model.predict_classes(test_scaled[])
@@ -49,13 +51,14 @@ def index():
 
 
 
-@app.route("/manufacturing/<location>")
-def manufacturing(location):
-    """Return the MetaData for a given location."""
-    # results = cur.execute(f"SELECT * FROM cacao_clean_withbean WHERE company_location='{location}'").fetchall()
-    # Create a dictionary entry for each row of metadata information
-    cacao_data = jsonify(cacao_table)
-    return cacao_data
+@app.route("/manufacturing")
+def manufacturingMain():
+    return render_template("manufacturing.html")
+
+   
+
+
+
 
 
 
